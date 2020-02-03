cb6f0bb1de0fded5b420c013cc580a86238eb19b
ryotaro tsuji <tsuji.xs@au.com>
2020-02-03 20:34:14 +0900

    Initial Commit

---
+++ slideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideshowApp
+//
+//  Created by Apple on 2020/02/03.
+//  Copyright © 2020 ryotaro.tsuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
